f9e749a41f686f7c3cbd0dcb1608db0a1e0db1fb
Wesley Azevedo <wesley150@MacBook-Air-Wesley.local>
2023-06-12 22:05:35 -0300

    good code equal bad code

--- good_code/src/App.vue
+++ good_code/src/App.vue
@@ -1,30 +1,163 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <v-app>
+      <div>
+        <v-app-bar color="#1c1c1c">
+          <template v-slot:prepend>
+            <v-img src="src/assets/TDC_LOGO.png" height="60" width="90" />
+            <span class="text-white">Pokédex - TDC Innovation 2023</span>
+          </template>
+        </v-app-bar>
+        <v-container class="mt-16">
+          <v-row>
+            <v-col cols="12">
+              <v-card class="mt-10">
+                <v-table
+                  density="compact"
+                  fixed-header
+                  height="300px"
+                  class="my-10"
+                >
+                  <thead>
+                    <tr>
+                      <th class="text-left">Nome</th>
+                      <th class="text-left">Ação</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-if="List1.length > 0"
+                      v-for="item in List1"
+                      :key="item.name"
+                    >
+                      <td>{{ item.name }}</td>
+                      <td>
+                        <v-btn
+                          @click="shw(item.url)"
+                          density="compact"
+                          icon="mdi-open-in-new"
+                          class="mr-2"
+                        ></v-btn>
+                        <v-btn
+                          @click="remove(item)"
+                          density="compact"
+                          icon="mdi-delete-outline"
+                          class="mr-2"
+                        ></v-btn>
+                      </td>
+                    </tr>
+                    <div v-else class="text-center mt-2">
+                      Adicione um pokemon para exibi-lo aqui
+                    </div>
+                  </tbody>
+                </v-table>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="9">
+              <v-select
+                v-model="current"
+                label="type2s"
+                :items="List2"
+                item-title="name"
+                return-object
+              ></v-select>
+            </v-col>
+            <v-col cols="3">
+              <v-btn height="55px" block color="primary" @click="add"
+                >Adicionar Pokémon</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-dialog v-model="show" width="auto">
+          <v-card>
+            <v-card-text>
+              <v-list lines="one">
+                <v-list-item>
+                  <h3 class="text-center">
+                    {{ current2?.name.toLocaleUpperCase() }}
+                  </h3>
+                </v-list-item>
+                <v-list-item>
+                  <v-img :src="current2?.photo"></v-img>
+                </v-list-item>
+                <v-list-item>
+                  <strong>Id:</strong><span>{{ current2?.id }}</span>
+                </v-list-item>
+                <v-list-item>
+                  <strong>Experiência:</strong><span>{{ current2?.xp }}</span>
+                </v-list-item>
+                <v-list-item>
+                  <strong>Altura:</strong><span>{{ current2?.height }}</span>
+                </v-list-item>
+                <v-list-item>
+                  <strong>Largura:</strong><span>{{ current2?.weight }}</span>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" block @click="show = false">Fechar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
+    </v-app>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+type type1 = {
+  name: string;
+  url: string;
+};
+
+type type2 = {
+  id: string;
+  name: string;
+  xp: string;
+  height: string;
+  weight: string;
+  photo: string;
+};
+
+const List1 = ref<type1[]>([]);
+const List2 = ref<type1[]>([]);
+const current = ref<type1>();
+const current2 = ref<type2>();
+const show = ref(false);
+
+const add = () => {
+  if (current.value) {
+    List1.value.push(current.value);
+  }
+};
+
+const remove = (i: type1) => {
+  const index = List1.value.findIndex((el) => el.name === i.name);
+  List1.value.splice(index, 1);
+};
+
+const shw = async (u: string) => {
+  const { data } = await axios.get(u);
+  const tmp = {
+    id: data.id,
+    name: data.name,
+    xp: data.base_experience,
+    height: data.height,
+    weight: data.weight,
+    photo: data.sprites.front_default,
+  };
+  current2.value = tmp;
+  show.value = true;
+};
+
+onMounted(async () => {
+  const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+  List2.value = data.results;
+});
+</script>
